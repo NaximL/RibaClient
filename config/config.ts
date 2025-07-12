ace4c03399dca2dce065c7657a5180d3a8213428
maximmim <maximloza0@gmail.com>
2025-07-12 20:17:21 +0300

    IS PROD

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 
-const ISPROD = false;
+const ISPROD = true;
 
 export const VERSION = 'v1.1.0';
 export const SERVER_URL = ISPROD ? 'https://26080284343b.ngrok-free.app' : 'http://192.168.1.146:3001';
