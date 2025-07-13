ca28e3112b358d67185fd283c7a037a5f6717c93
maximmim <maximloza0@gmail.com>
2025-07-13 11:47:48 +0300

    change url back

--- config/config.ts
+++ config/config.ts
@@ -2,4 +2,4 @@
 const ISPROD = true;
 
 export const VERSION = 'v1.2.1';
-export const SERVER_URL = ISPROD ? process.env.BACKEND_URL : 'http://192.168.1.146:3001';
+export const SERVER_URL = ISPROD ? "https://5ae84d151f2c.ngrok-free.app" : 'http://192.168.1.146:3001';
