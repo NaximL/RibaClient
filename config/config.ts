824d5b0f267888ad086000e9520f3f2223cc6955
maximmim <maximloza0@gmail.com>
2025-07-17 23:37:22 +0300

    Change config

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,5 @@
 
-export const ISPROD = false;
+export const ISPROD = true;
 
 export const VERSION = 'v1.5.7';
 export const SERVER_URL = ISPROD ? "https://fatsharkserv.online" : 'http://192.168.31.162:3001';
