61865f16dfcef24dafb1c084a25ee51928a7df30
maximmim <maximloza0@gmail.com>
2025-08-04 02:00:29 +0300

    prodaction mod

--- config/config.ts
+++ config/config.ts
@@ -1,6 +1,6 @@
 
-export const ISPROD = false;
+export const ISPROD = true;
 
-export const VERSION = 'v1.8.0';
+export const VERSION = 'v1.9.0';
 export const SERVER_URL = ISPROD ? "https://fatsharkserv.online" : 'http://192.168.31.162';
 
