e0a9a5bf3a0a90e54d3231a37ec5157b52e2a0e7
maximmim <maximloza0@gmail.com>
2025-08-06 00:52:26 +0300

    update status bar

--- styles/gstyles.ts
+++ styles/gstyles.ts
@@ -1,11 +1,27 @@
-import { StyleSheet, useColorScheme } from 'react-native';
+import { Platform,useColorScheme,StyleSheet } from 'react-native';
+import { useEffect } from 'react';
 
 export const Gstyle = () => {
   const isDark: boolean = useColorScheme() === 'dark';
 
+  const backgroundColor = isDark ? '#1a1a1f' : '#f7f7fa';
+
+
+  useEffect(() => {
+    if (Platform.OS === 'web') {
+      let metaThemeColor = document.querySelector("meta[name=theme-color]");
+      if (!metaThemeColor) {
+        metaThemeColor = document.createElement('meta');
+        metaThemeColor.setAttribute('name', 'theme-color');
+        document.head.appendChild(metaThemeColor);
+      }
+      metaThemeColor.setAttribute('content', backgroundColor);
+    }
+  }, [isDark]); 
+
   const gstyles = StyleSheet.create({
     back: {
-      backgroundColor: isDark ? '#1a1a1f' : '#f7f7fa',
+      backgroundColor,
     },
     WidgetBack: {
       backgroundColor: isDark ? '#1C1C1E' : '#fff',
@@ -20,22 +36,17 @@ export const Gstyle = () => {
       justifyContent: 'center',
     },
   });
+
   const Circle = isDark ? '#33333a' : 'rgba(255,255,255,0.9)';
   const WidgetColorText = isDark ? '#ffffff' : '#000000';
   const MessageTopicText = isDark ? '#ffffff' : '#1c1c1e';
   const ProfilText = isDark ? '#ffffff' : '#222';
   const ProfilTextValue = isDark ? '#ffffff' : '#444';
   const ProfilCircle = isDark ? '#33333a' : '#f2f2f7';
-
-
   const LoginText = isDark ? 'rgb(126 126 126)' : '#000000';
   const ChatText = isDark ? '#ffffff' : '#333333';
   const ChatTitle = isDark ? '#ffffff' : '#222222';
-
   const Login = isDark ? '#1d1d26' : '#fff';
-
-
-
 
   return {
     gstyles,
